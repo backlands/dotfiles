7f6b1af76048db2ab3222568ad2716d47a0ad95d
Eric Klempnauer <me@eric-klempnauer.com>
2019-12-11 13:01:09 -0700

    Rebuild rofi to support multiple script menus

---
+++ home/.config/rofi/scripts/powermenu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rofi_command="rofi -theme themes/powermenu.rasi"
+
+### Options ###
+power_off=""
+reboot=""
+lock=""
+suspend=""
+log_out=""
+# Variable passed to rofi
+options="$power_off\n$reboot\n$lock\n$suspend\n$log_out"
+
+chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 2)"
+case $chosen in
+    $power_off)
+        systemctl poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+    $lock)
+        sleep 1; ~/.config/scripts/lock.sh
+        ;;
+    $suspend)
+        sleep 1; ~/.config/scripts/lock.sh && systemctl suspend
+        ;;
+    $log_out)
+        i3-msg exit
+        ;;
+esac
+
