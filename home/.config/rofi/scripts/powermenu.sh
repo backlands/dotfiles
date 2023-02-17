489ffb7c5963a15d563bb66855d0c311d591bd57
Eric Klempnauer <me@eric-klempnauer.com>
2023-02-16 17:23:29 -0700

    fixup additional rofi menus

--- home/.config/rofi/scripts/powermenu.sh
+++ home/.config/rofi/scripts/powermenu.sh
@@ -65,7 +65,9 @@ run_cmd() {
 			amixer set Master mute
 			systemctl suspend
 		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+			if [[ "$DESKTOP_SESSION" == 'awesome' ]]; then
+				kill `pgrep awesome`
+			elif [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
 				openbox --exit
 			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
 				bspc quit
--- home/.config/rofi/scripts/scrotmenu.sh
+++ home/.config/rofi/scripts/scrotmenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rofi_command="rofi -theme themes/scrotmenu.rasi -no-show-icons"
+rofi_command="rofi -theme themes/scrotmenu.rasi -p Screenshot -mesg Screenshot -no-show-icons"
 
 ### Options ###
 screen=""
@@ -8,6 +8,14 @@ area=""
 window=""
 # Variable passed to rofi
 options="$screen\n$area\n$window"
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-p "Screenshot:" \
+		-mesg "Screenshot:" \
+		-theme $HOME/.config/rofi/themes/powermenu.rasi
+}
 
 chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 1)"
 case $chosen in
